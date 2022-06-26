9417b3a9d0462ba07275aa801ed79e5d54afabbf
Vuong Nguyen <vuongnguyen.170599@gmail.com>
2022-06-26 19:07:43 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const audioLinks = [
+  './audio/Da-Vu.mp3',
+  './audio/GapNhauBenNhauLaYTroi-Wind.mp3',
+  './audio/Bong-Hoa-Chang-Thuoc-Ve-Ta-Wind-Remix-Nhu-Viet-Deus.mp3',
+  './audio/CaoOc20-BRayDatGMasewKICM.mp3',
+  './audio/Cho-Qua-Lau.mp3',
+];
+const onShowCV = () => {
+  alert('Chưa có 😌');
+};
+const initAudio = () => {
+  var audio = document.getElementById('audio');
+  var source = document.getElementById('audioSource');
+  source.src = audioLinks[Math.floor(Math.random() * audioLinks.length)];
+
+  audio.muted = false;
+  audio.autoplay = true;
+  audio.load();
+  audio.play();
+};
+
+window.onload = () => {
+  initAudio();
+};
